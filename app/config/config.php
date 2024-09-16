e728b5cbd44362b30ea78961710d579b6629682c
LucasBreviglieri <Lucasbf.1.000@hotmail.com>
2024-09-16 19:00:49 -0300

    criando a estrutura do projeto

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('BASE', '/miniframework');
+
+
+
+define('UNSET_URI_COUNT', 1);
+define('DEBUG_URI', true);
+
+
+
+
+
+?>                              
---
+++ app/controller/TesteController.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace app\controller;
+
+class TesteController
+{
+    public function __construct()
+    {
+        echo 'estamos aqui';
+    }
+
+    public function seta(){
+        return ['nome' => 'XERECA'];
+    }
+
+}
+
+
+
+
+
+?>
